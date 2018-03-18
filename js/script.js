be08fd9b1fb61077838df14bbdfd13727a7e5654
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-18 15:01:50 +0100

    add constructor Column and createColumn function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 $(function() {
 
+  //ID GENERATOR
   function randomString() {
     const chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
     let str = '';
     for (let i = 0; i < 10 ; i++) {
-      str += chars[Math.floor(Math.random() * chars.length)];
+      str += chars[Math.floor(Math.random() * chars.length)]; // = np chars[12]
     }
     return str;
+  }
+
+  function Column(name) {
+    let self = this; //or const?
+    this.id = randomString();
+    this.name = name;
+    this.$element = createColumn();
+
+    // CREATE COLUMN FUNCTION
+    function createColumn() {
+      const $column = $('<div>').addClass('column');
+      const $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+      const $columnCardList = $('<ul>').addClass('column-card-list');
+      const $columnDelete = $('<button>')
+    }
   }
 
 }) // END $(document).ready()
